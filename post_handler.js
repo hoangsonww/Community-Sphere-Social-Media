ecc2e38ea71cc61ee5fd96287ce88a7288f661b5
Son Nguyen Hoang <124531104+hoangsonww@users.noreply.github.com>
2023-03-31 13:39:08 -0400

    Update post_handler.js

--- post_handler.js
+++ post_handler.js
@@ -1,1 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const postForm = document.getElementById('post-form');
+
+    if (postForm) {
+        postForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const postContent = document.getElementById('post-content').value;
+            addPostToPage(postContent);
+        });
+    }
+});
+
+function createPostElement(postContent) {
+    const postElement = document.createElement('div');
+    postElement.className = 'post';
+
+    const contentElement = document.createElement('p');
+    contentElement.textContent = postContent;
+    postElement.appendChild(contentElement);
+
+    return postElement;
+}
+
+function addPostToPage(postContent) {
+    const postElement = createPostElement(postContent);
+    const postListElement = document.getElementById('post-list');
+    postListElement.appendChild(postElement);
+}
 
